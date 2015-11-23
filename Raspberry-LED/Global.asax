9e9cf04c57f09468292a4db41a78ea6e50a66418
jelleloeve <jelle.loeve@hotmail.com>
2015-11-23 13:58:33 +0100

    Initial commit

---
+++ Raspberry-LED/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Raspberry_LED.MvcApplication" Language="C#" %>
---
+++ Raspberry-LED/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Raspberry_LED.Startup))]
+namespace Raspberry_LED
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
