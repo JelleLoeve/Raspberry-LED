14023b10aba60792772b775bb9159dfd3c07a1a8
jelleloeve <jelle.loeve@hotmail.com>
2015-11-23 14:28:41 +0100

    test comment

--- Raspberry-LED-Client/Program.cs
+++ Raspberry-LED-Client/Program.cs
@@ -10,6 +10,7 @@ namespace Raspberry_LED_Client
         static void Main(string[] args)
         {
             // Enter all the code for the Raspberry Pi
+            // Extra comment
         }
     }
 }
