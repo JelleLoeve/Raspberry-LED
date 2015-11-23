f4f00a0321c0fa8be44783e3df07e74459a6957f
jelleloeve <jelle.loeve@hotmail.com>
2015-11-23 14:16:51 +0100

    capital case in mathijs's comment: "Raspberry Pi"

--- Raspberry-LED-Client/Program.cs
+++ Raspberry-LED-Client/Program.cs
@@ -9,7 +9,7 @@ namespace Raspberry_LED_Client
     {
         static void Main(string[] args)
         {
-            // Enter all the code for the raspberry pi
+            // Enter all the code for the Raspberry Pi
         }
     }
 }
