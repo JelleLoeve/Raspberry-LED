8828c64feb1b63421e841a89c2d3bdb253463785
jelleloeve <jelle.loeve@hotmail.com>
2015-11-25 12:06:04 +0100

    made the nessesary controllers and views

---
+++ Raspberry-LED/Controllers/RasperryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Raspberry_LED.Controllers
+{
+    public class RasperryController : Controller
+    {
+        // GET: Rasperry
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Ping()
+        {
+            return View();
+        }
+    }
+}
---
+++ Raspberry-LED/Controllers/RouterController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Raspberry_LED.Controllers
+{
+    public class RouterController : Controller
+    {
+        // GET: Router
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Status()
+        {
+            return View();
+        }
+        public ActionResult Ping()
+        {
+            return View();
+        }
+    }
+}
