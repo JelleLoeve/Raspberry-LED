a0fb258fbee7f31e63bf471afa8af81787a5cb76
MathijsBesten <m88_db@hotmail.com>
2015-12-02 13:08:54 +0100

    submenu is working and styling is done

---
+++ Raspberry-LED/Scripts/side.js
@@ -0,0 +1,14 @@
+﻿var $ = jQuery;
+$(function () {
+    $('.nav li').hover(function () {
+        $(this)
+         .find('.submenu')
+         .stop(true, true)
+         .slideDown('fast');
+    }, function () {
+        $(this)
+         .find('.submenu')
+         .stop(true, true)
+         .slideUp('fast')
+    });
+});
