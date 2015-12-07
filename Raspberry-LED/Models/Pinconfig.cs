70387e8113570bfecb71836d7d9b3321ed4639f4
jelleloeve <jelle.loeve@hotmail.com>
2015-12-07 09:53:53 +0100

    Made a database for the pins.

---
+++ Raspberry-LED/Models/Pinconfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Raspberry_LED.Models
+{
+    public class Pinconfig
+    {
+        public int pinnumber { get; set; }
+
+        public string color { get; set; }
+
+        public bool isSet { get; set; }
+
+        public bool isOn { get; set; }
+
+    }
+    public class PinConfigDBContext : DbContext
+    {
+        public DbSet<Pinconfig> pinconfig { get; set; }
+
+    }
+}
