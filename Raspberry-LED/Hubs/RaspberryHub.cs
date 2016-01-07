5583c8df3f22e8540b3ed26c47da1cc36f04fc46
David Hijlkema <David.Hijlkema@gmail.com>
2016-01-07 12:34:11 +0100

    Unstable sync for other pc

---
+++ Raspberry-LED/Hubs/RaspberryHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Raspberry_LED.Helpers;
+
+namespace Raspberry_LED.Hubs
+{
+    [HubName("Raspberry")]
+    public class RaspberryHub : Hub
+    {
+        [HubMethodName("ChangeLed")]
+        public void ChangeLed(string ledIdInDb)
+        {
+            var SendToPi = new SocketSendAndRecieve("LED", ledIdInDb);
+            SendToPi.SendCommand();
+            if (SendToPi.GetRecievedData() == "error")
+                Clients.All.ErrorChangingLed(ledIdInDb);
+            else
+                Clients.All.ChangedLed();
+        }
+    }
+}
