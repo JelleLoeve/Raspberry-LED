d0e23950424b15b6c7917b31030a2ff4fd9e37eb
David Hijlkema <susiehond@hotmail.com>
2016-01-11 13:56:18 +0100

    Did some work on signalr stuff

--- Raspberry-LED/Hubs/RaspberryHub.cs
+++ Raspberry-LED/Hubs/RaspberryHub.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
 using Raspberry_LED.Helpers;
+using Raspberry_LED.Models;
+
 
 namespace Raspberry_LED.Hubs
 {
     [HubName("Raspberry")]
     public class RaspberryHub : Hub
     {
+        PinConfigDBContext db = new PinConfigDBContext();
         [HubMethodName("ChangeLedWeb")]
         public void ChangeLedWeb(string ledIdInDb)
         {
+            var dbData = db.PinConfigs.Find(int.Parse(ledIdInDb));
+            dbData.isOn = !dbData.isOn;
+            db.Entry(dbData).State = EntityState.Modified;
+            db.SaveChanges();
             Clients.Others.ChangePiLed(ledIdInDb);
         }
     }
